6ac486296b6e94d32a6102d77601b0453edeb61e
trevonromanuik <trevon.romanuik@gmail.com>
2026-04-14 09:00:15 -0600

    removed _ files and folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,15 @@ const srcAlias = new URL('./src', import.meta.url).pathname;
 export default defineConfig({
   outDir: './docs',
 	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					// This moves all assets (CSS, images, fonts) into a "css" folder
+					// [name] is the original filename, [hash] is for cache busting
+					assetFileNames: 'css/[name].[hash][extname]',
+				},
+			},
+		},
 		resolve: {
 			alias: {
 				'@': srcAlias,
